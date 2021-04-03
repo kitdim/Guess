66a4ef7af0ba8f1309b80d33d96f4ae45643f696
Дима <kitaykin9808@gmail.com>
2021-04-03 16:31:56 +0300

    Use func

--- main.py
+++ main.py
@@ -1,22 +1,28 @@
+def GuessNumber(a, b, i):
+    while True:
+        a += 1
+        b = int(input('Write the number: '))
+
+        if b == i:
+            print(f'You win!!!\n'
+                  f'Try № {a}')
+            break
+        elif b < i:
+            print(f'The number is bigger, try again\n'
+                  f'Try № {a}')
+        else:
+            print(f'The number is less, try again\n'
+                  f'Try № {a}')
+    else:
+        print('Done')
+
+
 print('Guess the number!!!')
 _try = 0
+number = 0
 guess = 15
 
-while True:
-    _try += 1
-    number = input('Write the number: ')
-    number = int(number)
+GuessNumber(_try, number, guess)
 
-    if number == guess:
-        print(f'You win!!!\n'
-              f'Try № {_try}')
-        break
-    elif number < guess:
-        print(f'The number is bigger, try again\n'
-              f'Try № {_try}')
-
-    else:
-        print(f'The number is less, try again\n'
-              f'Try № {_try}')
 
 
